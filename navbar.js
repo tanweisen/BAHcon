34492ca93853d413f0ff95ee5906fb7d16ab260e
tanweisen <tanweisen99@gmail.com>
2025-10-28 20:25:31 +0800

    ticket page update

--- navbar.js
+++ navbar.js
@@ -3,5 +3,22 @@ document.addEventListener("DOMContentLoaded", function () {
     .then(res => res.text())
     .then(data => {
       document.getElementById("navbar").innerHTML = data;
+
+      // Wait until navbar is added, then run scroll logic
+      const navbar = document.querySelector(".navbar");
+
+      // Start transparent if on top
+      if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+      }
+
+      // Change color when scrolling
+      window.addEventListener("scroll", function () {
+        if (window.scrollY > 50) {
+          navbar.classList.add("scrolled");
+        } else {
+          navbar.classList.remove("scrolled");
+        }
+      });
     });
 });
