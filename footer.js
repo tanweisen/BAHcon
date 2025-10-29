4bf79f8df1367e8c8920a05967da026f5279bf3a
Weemy96 <Weemy96@users.noreply.github.com>
2025-10-30 02:22:47 +0800

    fixed navbar
    +fixed navbar, almost same as figma
    +changed image of Envelope
    +fixed Envelope Hotel image layout inconsistency

--- footer.js
+++ footer.js
@@ -7,11 +7,11 @@
 // });
 
 const icons = {
-  arrow: `<svg width="7" height="11" viewBox="0 0 7 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+  arrow: `<svg width="7" height="11" viewBox="0 0 7 11" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M1.29999 0C3.49999 1.3 5.29996 3.2 6.39996 5.5C5.29996 7.8 3.49999 9.7 1.29999 11C0.499988 9.3 0 7.5 0 5.5C0 3.6 0.399988 1.7 1.29999 0Z"
-                fill="#FFFBE8" />
-            </svg>`
+              />
+          </svg>`
 };
 
 document.addEventListener("DOMContentLoaded", function () {
--- navbar.js
+++ navbar.js
@@ -1,8 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const icons = {
+  arrow: `<svg width="7" height="11" viewBox="0 0 7 11" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M1.29999 0C3.49999 1.3 5.29996 3.2 6.39996 5.5C5.29996 7.8 3.49999 9.7 1.29999 11C0.499988 9.3 0 7.5 0 5.5C0 3.6 0.399988 1.7 1.29999 0Z"
+            />
+          </svg>`
+};
+
   fetch("navbar.html")
     .then(res => res.text())
     .then(data => {
       document.getElementById("navbar").innerHTML = data;
+
+      document.querySelectorAll("#navbar [data-icon]").forEach(el => {
+        const name = el.getAttribute("data-icon");
+        if (icons[name]) el.innerHTML = icons[name];
+      });
 
       // Wait until navbar is added, then run scroll logic
       const navbar = document.querySelector(".navbar");
